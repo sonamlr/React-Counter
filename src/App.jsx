e242baefd8863d30c0ec690ed48a7fe44276cae0
Sonam <sonamlodhi6251@gmail.com>
2024-01-08 23:40:41 +0530

    add condition with counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import './App.css'
 
 function App() {
  const [count, setCounter] = useState(0);
+ const incrementCount = () => {
+  if(count < 20){
+    setCounter(count + 1)
+  }
+ }
+ const decrementCount = () =>{
+  if(count > 0){
+    setCounter(count - 1)
+  }
+ }
 
   return (
     <>
      <h1 className='text-3xl font-bold underline'>Tailwind CSS</h1>
      <div>{count}</div>
-     <button onClick={() => setCounter(count + 1)}>Increment</button>
-     <button onClick={() => setCounter(count - 1)}>Decrement</button>
+     <button onClick={incrementCount}>Increment</button>
+     <button onClick={decrementCount}>Decrement</button>
 
     </>
   )
